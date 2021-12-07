f108210eea3d9bcfa6d63d777036a0dcb0097805
haju <haju0321@gmail.com>
2021-12-07 16:55:06 +0900

    Initial Commit

---
+++ MyRecipes/MyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipesApp.swift
+//  MyRecipes
+//
+//  Created by haju Kim on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
